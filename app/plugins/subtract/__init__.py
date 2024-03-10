932cbf8ecac057eb2714d65f14cac869ba19d0f3
gvr1220 <gv225@njit.edu>
2024-03-09 22:24:43 -0500

    Modified to handle operations with more than two numbers

--- app/plugins/subtract/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.commands.operation_command import OperationCommand
-from calculator.operations import subtract
-class SubtractCommand(OperationCommand):
-    def __init__(self):
-        super().__init__(subtract)
